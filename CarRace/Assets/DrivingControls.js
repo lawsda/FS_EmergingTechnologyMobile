8346032b8305964023a23368e0b8baf64c6e2746
lawsda <lawsda01@gmail.com>
2014-04-08 19:01:15 -0400

    Built car modeland basic track

---
+++ CarRace/Assets/DrivingControls.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var forwardSpeed : float = 250;
+var turnSpeed : float = 2;
+
+function Start () {
+
+}
+
+function Update () {
+
+	var forwardAmount = Input.GetAxis("Vertical") * forwardSpeed;
+	var turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
+	
+	transform.Rotate(0,turnAmount,0);
+	rigidbody.AddRelativeForce(forwardAmount,0,0);
+	
+}
---
+++ CarRace/Assets/Wheels.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+ 
+var turned = false;
+var turnedRight = false;
+var turnedLeft = false;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+
+	var turnDir = Input.GetAxis("Horizontal");
+	
+	if(!turned && turnDir > 0){
+		transform.Rotate(0,10,0);
+		turned = true;
+		turnedRight = true;
+	}
+	else if(!turned && turnDir < 0){
+		transform.Rotate(0,-10,0);
+		turned = true;
+		turnedLeft = true;
+	}
+	else if(turned && turnDir == 0){
+		if(turnedRight){
+			transform.Rotate(0,-10,0);
+			turnedRight = false;
+		}
+		else if(turnedLeft){
+			transform.Rotate(0,10,0);
+			turnedLeft = false;
+		}
+		turned = false;
+	}	
+
+}
+
+
