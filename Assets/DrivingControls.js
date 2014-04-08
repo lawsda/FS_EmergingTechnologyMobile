ba06a3fd7f8be15d93f8711d7fcb50a945b7c6e1
lawsda <lawsda01@gmail.com>
2014-04-08 01:10:22 -0400

    v1.0 Car Race

---
+++ Assets/DrivingControls.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var forwardSpeed : float = 40;
+var turnSpeed : float = 2;
+
+function Start () {
+
+}
+
+function Update () {
+
+	var forwardAmount = Input.GetAxis("Vertical") * forwardSpeed;
+	var turnAmount = Input.GetAxis("Horizontal") * turnSpeed;
+	
+	transform.Rotate(0,turnAmount,0);
+	rigidbody.AddRelativeForce(0,0,forwardAmount);
+	
+}
---
+++ Assets/Wheels.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+
+function Start () {
+
+}
+
+function Update () {
+	var rotationAmount = Input.GetAxis("Vertical") * 5;
+	transform.Rotate(0, rotationAmount, 0);
+}
